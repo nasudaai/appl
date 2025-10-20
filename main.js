b3ef9de7e6123243415e74277ae75772aeb8ebc8
nasuda <nasuda908@gmail.com>
2025-10-20 20:36:51 +0900

    add newcode main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const arr = [1, 2, 3, 4];
 
 const initialValue = 0;
 
-
+//reduce
 const sum = arr.reduce(
   (acc, cur) => acc + cur, initialValue
 );
@@ -14,6 +14,10 @@ console.log(sum);
 const mapped = arr.map((x) => x * 2);
 
 console.log(mapped);
+
+//filter
+const result = arr.filter((n) => n % 2 === 0);
+console.log(result);
 
 function main() {
   console.log("main");
