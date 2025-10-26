35111bdeca2e3789cf6d2d4d1c55e8db1365f4bd
nasuda <nasuda908@gmail.com>
2025-10-27 04:30:39 +0900

    modified today.sh search code

--- memoapp/today.sh
+++ memoapp/today.sh
@@ -3,3 +3,28 @@
 today=$(date "+%Y-%m-%d")
 
 printf "$today\n"
+
+current_line=""
+date_regex='^[0-9]{4}-[0-9]{2}-[0-9]{2}$'
+
+for word in $(cat out.txt); do
+
+    if [ -z "$current_line" ]; then
+        current_line="$word"
+    else
+        current_line="$current_line $word"
+    fi
+
+
+    if [[ $word =~ $date_regex ]]; then
+        if [ $word = $today ]; then
+
+            echo  "$current_line"
+            current_line=""
+        else
+            current_line=""
+        fi
+    
+    fi
+
+done
