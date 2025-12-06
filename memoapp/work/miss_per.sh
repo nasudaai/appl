ec77ec4090953025972916070453c4815de1220a
nasuda <nasuda908@gmail.com>
2025-12-06 10:22:04 +0900

    miss percentage function

--- memoapp/work/miss_per.sh
+++ memoapp/work/miss_per.sh
@@ -8,5 +8,18 @@ percentage=$((miss * 10000 / total))
 int_part=$(( percentage / 100 ))
 frac_part=$(( percentage % 100 ))
 
+func() {
+
+    s=$1
+    m=$2
+    t=$(( $s + $m ))
+    n=$(( $m * 10000 / $t ))
+    i=$(( $n / 100 ))
+    f=$(( $n % 100 ))
+    echo "$i.$f%"
+}
+
+#p=$(func $1 $2)
+#echo $p
 #printf で書き直そうかな
 echo "$int_part.$frac_part%"
