7ad5b8c5a135d336ef91ac2b487f9d4700af0dca
nasuda <nasuda908@gmail.com>
2025-12-07 00:51:33 +0900

    update insert

--- memoapp/work/insert_yen.sh
+++ memoapp/work/insert_yen.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+source ./fmiss_perc.sh
+p=$(func $2 $4)
+
+
 all=$@
 types="$2"
 miss="$4"
+
 
 
 
@@ -12,11 +17,15 @@ echo "arguments: $#"
 
 if [ $# -eq 5 ]; then
 
+    
 
     echo "$@ $(date +"%Y-%m-%d %H:%M:%S")" >> out/out.txt
+    echo "correct keystrokes: $types miss: $miss all types: $(( $types + $miss )) per: $p"
+#    ./miss_per.sh $types $miss
+
+
     echo "last 5 lines will be displayed. "
     cat out/out.txt | tail -n 5
-    echo "all types: $(( $types + $miss ))"
 else
 
     echo "5 arguments are required."
