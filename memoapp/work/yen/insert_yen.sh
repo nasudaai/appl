df1b6bde21b8c7ce9054e1f922dcb2943705407e
nasuda <nasuda908@gmail.com>
2025-12-15 17:32:50 +0900

    update insert

--- memoapp/work/yen/insert_yen.sh
+++ memoapp/work/yen/insert_yen.sh
@@ -7,6 +7,13 @@ date=$(date +"%Y-%m-%d %H:%M:%S")
 miss="$4"
 all_keystroke=$(( $2 + miss ))
 
+#calulation percetange miss
+
+result=$(( miss * 10000 / all_keystroke ))
+int_per=$(( result / 100 ))
+f_per=$(( result % 100 ))
+perc="$int_per.$f_per%"
+
 echo "$date"
 echo "args: $count_arg"
 
@@ -15,7 +22,9 @@ if [ $count_arg -eq 5 ]; then
 
     
     echo "$@ $all_keystroke $date" >> out/out.txt
-    printf "%s\n" "$@"
+    printf "%s " "$@" "$all_keystroke" "$perc"
+    printf "\n"
+#    printf "%s\n" "$@"
 
 elif [ $count_arg -eq 0 ]; then
 
