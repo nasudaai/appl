ba16b522ea5cacb949405fefa70c8b83f6a9a352
nasuda <nasuda908@gmail.com>
2025-12-17 16:21:49 +0900

    create url

--- server-app/creatpath.mjs
+++ server-app/creatpath.mjs
@@ -8,3 +8,6 @@ const __dirname = path.dirname(__filename);
 const filePath = path.join( __dirname, "txt/txt.md");
 console.log(filePath);
 
+const txt = readFileSync(filePath, "utf-8");
+console.log(txt);
+
