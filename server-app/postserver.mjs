04b3a6adf9636b9e6e0d7ebfa93d5f2b76a1f017
nasuda <nasuda908@gmail.com>
2025-12-19 00:48:06 +0900

    create post server

---
+++ server-app/postserver.mjs
@@ -0,0 +1,31 @@
+import http from 'node:http';
+
+http.createServer((req, res) => {
+
+  if (req.method === "POST") {
+
+    let body = "";
+
+
+    req.on("data", chunk => {
+
+      body += chunk;
+    });
+
+
+    req.on("end", () => {
+      
+      res.writeHead(200, { "content-Type": "text/plain" });
+      res.end(`received: ${body}\n`);
+    });
+
+    return;
+  }
+
+
+  res.writeHead(200, { "content-Type": "text/plain" });
+  res.end("hello\n");
+
+}).listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
