11072b32421ac176a331c3eaf914672944e8d015
nasuda <nasuda908@gmail.com>
2025-12-22 07:14:59 +0900

    add plans

--- memoapp/work/yen/show_cource.sh
+++ memoapp/work/yen/show_cource.sh
@@ -3,7 +3,7 @@
 read -p "cource? " cource
 
 
-# case で分岐 3 5 10
+# branching in the 3 5 10 cases.
 while read -r f1 f2 f3 f4 f5 f6; do
-    [ "$f5" -eq $cource ] && echo "$f1 $f2 $f3 $f4 $f5 $f6"
+    [ "$f5" = $cource ] && echo "$f1 $f2 $f3 $f4 $f5 $f6"
 done < out/out.txt
