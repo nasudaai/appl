89ea1aa336bf7b9dcba2488937ce7a349d7a5597
nasuda <nasuda908@gmail.com>
2025-12-22 07:08:53 +0900

    read cource number

--- memoapp/work/yen/show_cource.sh
+++ memoapp/work/yen/show_cource.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+read -p "cource? " cource
 
+
+# case で分岐 3 5 10
 while read -r f1 f2 f3 f4 f5 f6; do
-    [ "$f5" -eq 3 ] && echo "$f1 $f2 $f3 $f4 $f5 $f6"
+    [ "$f5" -eq $cource ] && echo "$f1 $f2 $f3 $f4 $f5 $f6"
 done < out/out.txt
