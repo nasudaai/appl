1d572bb5c10ae21dc716a4d44284619ea5dbf478
nasuda <nasuda908@gmail.com>
2025-12-25 10:02:20 +0900

    cource condhition

--- memoapp/work/yen/show_cource.sh
+++ memoapp/work/yen/show_cource.sh
@@ -4,6 +4,7 @@ read -p "cource? " cource
 
 
 # branching in the 3 5 10 cases.
+# tail -n 5 |
 while read -r f1 f2 f3 f4 f5 f6; do
     [ "$f5" = $cource ] && echo "$f1 $f2 $f3 $f4 $f5 $f6"
 done < out/out.txt
