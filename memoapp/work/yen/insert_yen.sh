4702df0510ab2b80b2b3ba2c09b767f81fcbc701
nasuda <nasuda908@gmail.com>
2025-12-27 13:23:16 +0900

    update

--- memoapp/work/yen/insert_yen.sh
+++ memoapp/work/yen/insert_yen.sh
@@ -17,7 +17,8 @@ args=("$@")
 printf "%s\n" "yen: ${args[0]}"
 printf "%s\n" "correct keystrokes: ${args[1]}"
 printf "%s\n" "types par second: ${args[2]}"
-printf "%s" "miss: ${args[3]}"
+printf "%s\n" "miss: ${args[3]}"
+printf "%s" "cource: ${args[4]}"
 printf "\n"
 
 date=$(date +"%Y-%m-%d %H:%M:%S")
