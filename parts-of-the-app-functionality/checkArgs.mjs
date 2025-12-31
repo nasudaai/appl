8006518b4b235826f9a8a0b7054957efab8901b9
nasuda <nasuda908@gmail.com>
2025-12-31 12:23:42 +0900

    add err

--- parts-of-the-app-functionality/checkArgs.mjs
+++ parts-of-the-app-functionality/checkArgs.mjs
@@ -1,4 +1,11 @@
 const args = process.argv.slice(2);
 
 const [file, title] = args;
-process.stdout.write(file + " " + title);
+
+if (!file || !title) {
+  process.stderr.write("usage: please <filename> <title>\n")
+  process.exit(1);
+} else {
+  
+  process.stdout.write(file + " " + title + "\n");
+}
